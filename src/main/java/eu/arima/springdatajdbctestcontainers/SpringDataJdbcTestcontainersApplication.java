e7af8d1aa6c5035f0ef7040873683f9869b6e784
jessica <jessica@arima.eu>
2020-04-23 14:56:22 +0200

    actualización dependencias de spring-data-commons y spring-data-jdbc

--- src/main/java/eu/arima/springdatajdbctestcontainers/SpringDataJdbcTestcontainersApplication.java
+++ src/main/java/eu/arima/springdatajdbctestcontainers/SpringDataJdbcTestcontainersApplication.java
@@ -2,26 +2,11 @@ package eu.arima.springdatajdbctestcontainers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.data.jdbc.repository.config.JdbcConfiguration;
-import org.springframework.data.mapping.PersistentEntity;
-import org.springframework.data.mapping.PersistentPropertyAccessor;
-import org.springframework.data.mapping.PreferredConstructor;
-import org.springframework.data.mapping.context.MappingContext;
-import org.springframework.data.relational.core.conversion.BasicRelationalConverter;
 import org.springframework.data.relational.core.conversion.RelationalConverter;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
-import org.springframework.data.relational.core.mapping.RelationalPersistentEntity;
-import org.springframework.data.relational.core.mapping.RelationalPersistentProperty;
-import org.springframework.data.util.TypeInformation;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
-import java.util.function.Function;
 
 /**
  * {@link JdbcConfiguration} registers {@link RelationalMappingContext} and {@link RelationalConverter} for Spring Data
